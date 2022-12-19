b16870aa2d39ef558b64c5fe9dbacfeee30084c8
dijialei <dijialei0605@gmail.com>
2022-12-19 12:14:18 +0100

    MAJ

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors=require('cors');
 const app = express();
 const User = require('./models/users');
 const Absence = require('./models/absences');
@@ -9,6 +10,17 @@ mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/gestiondb');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+/* app.use(cors());
+app.all('*', function (req,res){
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+    res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', "GET, PUT, DELETE, POST");
+    req.next();
+}); */
+
+
+
+
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -18,13 +30,8 @@ app.use(function (req, res, next) {
 
 //增删改操作加await会出错，不加有时会读取不准确
 app.post('/updateUser', async(req,res)=>{
-    User.findByIdAndUpdate(req.body._id, req.body, function (err, docs) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Updated Object:", docs);
-        }
-    });
+    let result= await User.findByIdAndUpdate(req.body._id, req.body);
+    res.send(result);
 });
 
 app.post('/verification', async (req, res) => {
@@ -40,7 +47,11 @@ app.post('/absence', async(req,res)=>{
 });
 
 app.post('/createAbsence', async (req, res) => {
-    Absence.create(req.body, (error, absence) => { console.log(error, absence) });
+   let result = await Absence.create(req.body);
+   res.send(result);
+    
+
+   
 });
 app.post('/check', async(req,res)=>{
     let result = await JourFerie.find(req.body);
@@ -48,14 +59,8 @@ app.post('/check', async(req,res)=>{
     res.send(result[0]);
 });
 app.post('/deleteAbsence', async(req,res)=> {    
-     Absence.findByIdAndDelete(req.body._id, function (err, docs) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Deleted Object:", docs);
-        }
-
-    });
+    let result= await Absence.findByIdAndDelete(req.body._id);
+    res.send(result);
     
     /* let result = await Absence.find({userId:req.body.userId}).exec();
     res.json(result); */
